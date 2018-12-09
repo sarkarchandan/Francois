2476942ac2f319b2d0eeb5bcf3dc21995981b021
chandan <elitegreen@gmail.com>
2018-12-09 03:50:14 +0100

    Refactored the Helper as MatrixAdjunct header

--- tests/algebra_Matrix_AnalyticalTests.cpp
+++ tests/algebra_Matrix_AnalyticalTests.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "Matrix.hpp"
-#include "Helper.hpp"
+#include "MatrixAdjunct.hpp"
 
 TEST(MatrixAnalyticalTests, canDefineRowMatrix)
 {
--- tests/algebra_Matrix_SanityTests.cpp
+++ tests/algebra_Matrix_SanityTests.cpp
@@ -1,6 +1,6 @@
 #include "gtest/gtest.h"
 #include "Matrix.hpp"
-#include "Helper.hpp"
+#include "MatrixAdjunct.hpp"
 #include <memory>
 
 TEST(MatrixSanityTests, canDetermineMatrixValidity)
